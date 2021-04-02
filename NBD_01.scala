23a9afae4bab549760e9c37d264d76b499405d51
Pawel Celejewski <s15799@pjwstk.edu.pl>
2021-04-02 13:15:05 +0200

    applying fixes after code review, fix recursion methods
    Signed-off-by: Pawel Celejewski <s15799@pjwstk.edu.pl>

--- NBD_01.scala
+++ NBD_01.scala
@@ -1,3 +1,4 @@
+import scala.annotation.tailrec
 import scala.collection.mutable.ListBuffer
 
 object NBD_01 {
@@ -8,7 +9,7 @@ object NBD_01 {
     def main(args: Array[String]) = {
 
 
-      val daysOfWeek = Array("Poniedzialek", "Wtorek", "Sroda", "Czwartek", "Piatek", "Sobota", "Niedziela")
+      val daysOfWeek = List("Poniedzialek", "Wtorek", "Sroda", "Czwartek", "Piatek", "Sobota", "Niedziela")
       val separator = ","
 
       val result_1a = concatUsingForLoop(daysOfWeek, separator)
@@ -25,17 +26,17 @@ object NBD_01 {
       println("\nZadanie 1c")
       println(result_1c)
 
-      val result_2a = concatUsingRecursion(0, daysOfWeek)
+      val result_2a = concatUsingRecursion(daysOfWeek)
       println("\nZadanie 2a")
       println(result_2a)
 
 
-      val result_2b = concatUsingRecursionReverse(daysOfWeek.size - 1, daysOfWeek)
+      val result_2b = concatUsingRecursionReverse(daysOfWeek)
       println("\nZadanie 2b")
       println(result_2b)
 
 
-      val result_3 = concatUsingTailRecursion(1, daysOfWeek, daysOfWeek(0))
+      val result_3 = concatUsingTailRecursion(daysOfWeek)
       println("\nZadanie 3")
       println(result_3)
 
@@ -62,12 +63,12 @@ object NBD_01 {
 
       println("\nZadanie 8")
       val inputList_8 = List[Int](3,5,0,1,4,0,1)
-      val result_8 = removeZerosFromList(0, inputList_8, new ListBuffer[Int])
+      val result_8 = removeZerosFromList(inputList_8)
       println("Input List: " + inputList_8)
       println("Output List: " + result_8)
 
       println("\nZadanie 9")
-      val result_9 = incrementListElemets(List[Int](3,5,0,1,4,0,1))
+      val result_9 = incrementListElements(List[Int](3,5,0,1,4,0,1))
       println("Output List")
       println(result_9)
 
@@ -79,7 +80,7 @@ object NBD_01 {
 
 
     // zadanie 1 a)
-    def concatUsingForLoop(daysOfWeek: Array[String], separator: String): String ={
+    def concatUsingForLoop(daysOfWeek: List[String], separator: String): String ={
 
       var result = ""
 
@@ -94,16 +95,13 @@ object NBD_01 {
 
 
   // zadanie 1 b)
-  def concatUsingForLoopWithFilter(daysOfWeek: Array[String], separator: String): String ={
+  def concatUsingForLoopWithFilter(daysOfWeek: List[String], separator: String): String ={
 
     var result = ""
 
-    for (item <- daysOfWeek){
-      if (item.toUpperCase.startsWith("P")) {
-        result = result.concat(item).concat(separator)
-      }
+    for (item <- daysOfWeek if item.toUpperCase.startsWith("P")){
+      result = result.concat(item).concat(separator)
     }
-
     result = result.dropRight(1)
 
     return result
@@ -112,7 +110,7 @@ object NBD_01 {
 
 
   // zadanie 1 c)
-  def concatUsingWhileLoop(daysOfWeek: Array[String], separator: String): String ={
+  def concatUsingWhileLoop(daysOfWeek: List[String], separator: String): String ={
 
     var result = ""
 
@@ -130,46 +128,44 @@ object NBD_01 {
 
 
   // zadanie 2 a)
-  def concatUsingRecursion(idx: Int, daysOfWeek: Array[String]): String ={
+  def concatUsingRecursion(daysOfWeek: List[String]): String ={
 
     val separator: String = ","
-
-    if (idx == daysOfWeek.size){
+    if (daysOfWeek.isEmpty){
         return ""
+    } else {
+      return daysOfWeek.head + separator + concatUsingRecursion(daysOfWeek.tail)
     }
 
-    val new_idx = idx + 1
-    return daysOfWeek(idx) + separator + concatUsingRecursion(new_idx, daysOfWeek)
   }
 
 
   // zadanie 2 b)
-  def concatUsingRecursionReverse(idx: Int, daysOfWeek: Array[String]): String ={
+  def concatUsingRecursionReverse(daysOfWeek: List[String]): String ={
 
     val separator: String = ","
 
-    if (idx < 0){
+    if (daysOfWeek.isEmpty){
       return ""
+    } else {
+      return concatUsingRecursionReverse(daysOfWeek.tail) + separator + daysOfWeek.head
     }
-
-    val new_idx = idx - 1
-    return daysOfWeek(idx) + separator + concatUsingRecursionReverse(new_idx, daysOfWeek)
   }
 
 
   // zadanie 3
-  def concatUsingTailRecursion(idx: Int, daysOfWeek: Array[String], acumulator: String): String ={
+  def concatUsingTailRecursion(daysOfWeek: List[String]): String ={
 
     val separator: String = ","
 
-    if (idx == daysOfWeek.size){
-      return acumulator;
-    } else{
-      val acc = acumulator + separator + daysOfWeek(idx)
-      val next_idx = idx + 1
-      return concatUsingTailRecursion(next_idx, daysOfWeek, acc)
+    @tailrec
+    def concat(daysOfWeek: List[String], accumulator: String): String = {
+      daysOfWeek match {
+        case Nil => accumulator
+        case daysOfWeek => concat(daysOfWeek.tail, accumulator + separator + daysOfWeek.head)
+      }
     }
-
+    concat(daysOfWeek, "")
   }
 
 
@@ -178,7 +174,7 @@ object NBD_01 {
   //  List(1, 3, 8).foldRight(100)(_ - _) == 1 - (3 - (8 - 100)) == -94
 
   // zadanie 4a
-  def concatUsingFoldLeft(daysOfWeek: Array[String]): String ={
+  def concatUsingFoldLeft(daysOfWeek: List[String]): String ={
 
     val separator: String = ","
 
@@ -188,7 +184,7 @@ object NBD_01 {
 
 
   // zadanie 4b
-  def concatUsingFoldRight(daysOfWeek: Array[String]): String ={
+  def concatUsingFoldRight(daysOfWeek: List[String]): String ={
 
     val separator: String = ","
 
@@ -198,12 +194,10 @@ object NBD_01 {
 
 
   // zadanie 4c
-  def concatUsingFoldLeftWithFilter(daysOfWeek: Array[String]): String ={
-
+  def concatUsingFoldLeftWithFilter(daysOfWeek: List[String]): String ={
     val separator: String = ","
 
-    return daysOfWeek.foldLeft(""){ (sum, item) => if(item.toUpperCase.startsWith("P")){sum + item + separator} else sum + "" }
-
+    return daysOfWeek.filter(_.toUpperCase.startsWith("P")).foldLeft(""){ (sum, item) =>  {sum + item + separator}}
   }
 
 
@@ -214,10 +208,11 @@ object NBD_01 {
 
     println("Original products map: " + products)
 
-    val productsAfterDiscount = products.map{ case (key, value) => (key, value * 0.9) }
+    // val productsAfterDiscount = products.map{ case (key, value) => (key, value * 0.9) }
+    // consider mapValues()
+    val productsAfterDiscount = products.mapValues{ _ * 0.9 }
 
     println("Altered products map: " + productsAfterDiscount)
-
   }
 
   // zadanie 6
@@ -235,43 +230,45 @@ object NBD_01 {
   def testOptionUsage(): Unit ={
     val products = Map("mleko" -> 4.00, "chleb" -> 2.5, "cukier" -> 5.25)
 
-
     println("Operacja get na mapie zwaraca obiekt typu Option, ktora posiada obiekt Some z wartoscia lub None")
     println(products.get("chleb"))
     println("Po odpakowaniu: " + products.get("chleb").get)
     println(products.get("maslo"))
+    println(products.getOrElse("maslo", ""))
+    println(products.isEmpty)
     println("Czy zwrocona wartosc jest typu Option?: " + products.get("chleb").isInstanceOf[Option[Double]])
     println("Czy zwrocona wartosc jest typu Option?: " + products.get("maslo").isInstanceOf[Option[Double]])
 
   }
 
 
-
   // zadanie 8
-  def removeZerosFromList(idx: Int, inputList: List[Int], accumulator: ListBuffer[Int]): List[Int] ={
+  def removeZerosFromList(inputList: List[Int]): List[Int] ={
 
-    if (idx == inputList.size){
-      return accumulator.toList
-    } else{
-
-      if (inputList(idx) != 0){
-        accumulator.append(inputList(idx))
+    @tailrec
+    def removeZeros(inputList: List[Int], accumulatorList : ListBuffer[Int]) : List[Int] = {
+      inputList match {
+        case Nil => accumulatorList.toList
+        case inputList => {
+          if (inputList.head != 0) {
+            accumulatorList.append(inputList.head)
+          }
+          removeZeros(inputList.tail, accumulatorList)
+        }
       }
-      val new_idx = idx + 1
-
-      removeZerosFromList(new_idx, inputList, accumulator)
     }
 
+    removeZeros(inputList, ListBuffer())
   }
 
 
   // zadanie 9
-  def incrementListElemets(inputList : List[Int]): List[Int] ={
+  def incrementListElements(inputList : List[Int]): List[Int] ={
 
     println("Input List")
     println(inputList)
 
-    return inputList.map(x => x +1)
+    return inputList.map(x => x + 1)
   }
 
 
